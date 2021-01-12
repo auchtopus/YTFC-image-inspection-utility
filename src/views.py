622ccdc49bb58d1f84c75e81ab434294f792843c
Anthony Jiang <anthony.jiang@yale.edu>
2021-01-12 16:43:34 -0600

    fixed backend, dataset loading/caching, basic queries now work

--- src/views.py
+++ src/views.py
@@ -1,25 +1,9 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from collections import defaultdict
+from collections import defaultdict, Counter
 
-from datasets import Dataset
-
-NEVP_2019_09_10_bindings = {"catalog_number":  "catalogNumber", # this functions as the primary key
-                            "url": "originalurl",
-                            "sci_name": "scientificName",
-                            "family": "family",
-                            "order": None,
-                            "inst": "institutionCode",
-                            "object_id": "occid",
-                            "state": "stateProvince",
-                            "event_date": "eventDate",
-                            "year": "year",
-                            "county": "county",
-                            "municipality": "municipality",
-                            "locality": "locality",
-                            "latitude": "decimalLatitude",
-                            "longitue": "decimalLongitude"}
+from datasets import Metric, Dataset
 
 
 class CacheDataset(Dataset):
@@ -30,37 +14,34 @@ class CacheDataset(Dataset):
     def __init__(self, label_map):
         super().__init__(label_map)
 
-    @st.cache
     def load_master_dataset(self, csv_path):
         return super().load_master_dataset(csv_path)
 
-    @st.cache
     def load_gt(self, gt_csv_path, status_list):
         return super().load_gt(gt_csv_path, status_list)
 
-    @st.cache
     def load_orders(self, order_csv_path, head_label=True):
         return super().load_orders(order_csv_path, head_label=head_label)
 
-    @st.cache
     def load_preds(self, pred_csv_path, status_list, binarized=False):
         return super().load_preds(pred_csv_path, status_list, binarized=binarized)
 
-    @st.cache
     def merge_preds_gt(self, preds_df, gt_df):
         return super().merge_preds_gt(preds_df, gt_df)
     
 
-class Dataview(CacheDataset): #TODO determine where to put queries
-
+class Dataview(Dataset): #TODO determine where to put queries
+    
+    # @st.cache(allow_output_mutation=True)
     def __init__(self, status_list, label_map, master_dataset_path, order_csv_path):
         super().__init__(label_map)
         super().load_master_dataset(master_dataset_path) # this gives us self.master_df
         super().load_orders(order_csv_path)
         self.fields = self.master_df.columns.values
+        print(f"loading data from {master_dataset_path}")
         
         
-
+    # @st.cache(allow_output_mutation=True)
     def load_prediction_set(self, status_list, gt_csv_path, pred_csv_path):
         """
         There can be multiple gt/pred combinations appended to a single master list.
@@ -82,30 +63,48 @@ class Dataview(CacheDataset): #TODO determine where to put queries
              {status} Prediction Confidence: np.linspace,
              metrics: ['Accuracy', 'F1-score','Precision', 'Recall',True Positive', 'False Positive', 'True Negative', 'False Negative']}
         """
-        if f"{query['status']} Prediction" not in self.fields:
-            raise IndexError(f"No predictions for status: {query['status']}")
+
+        print(query)
 
 
-        # work through the entries to build the query. Would sql be faster?
-        mask = pd.Series([True] * len(self.master_df))
-        if query['family'] != '-':
-            mask = mask & (self.master_df["family"].isin(query["family"]))
-        if query['order'] != '-':
-            mask = mask & (self.master_df["family"].isin(query["family"]))
-        if query[f"{query['status']} Prediction"] != '-':
-            mask = mask & (self.master_df[f"{query['status']} Prediction"] == f"{query['status']} Prediction")
-        if query[f"{query['status']} Prediction Confidence"] != '-':
-            mask = mask & (self.master_df[f"{query['status']} Prediction Confidence"] > query[f"{query['status']} Prediction Confidence"])
-        if query[f"{query['status']} Prediction"]:
-            def filter_conf_state(row, pred, gt):
-                return row[f"{query['status']} Prediction"] == pred and row[f"{query['status']} Ground Truth" ] == gt
-            state_dict = {"True Positive": [True, True],
-                          "False Positive": [True, False],
-                          "True Negative": [False, False],
-                          "False Negative": [False, True]}
-            for confusion_state in query["confusion_state"]:
-                state = state_dict[confusion_state]
-                mask = mask & (self.master_df.apply(filter_conf_state, axis = 1, pred = state[0], gt = state[1]))
+        mask = pd.Series([True] * len(self.master_df), index = self.master_df.index)
+        st.write(f"base_filter: {Counter(mask)}")
+
+        # check for status
+        if query['status'] != "All" and len(query['status']) > 0:
+            if f"{query['status']} Prediction" not in self.fields:
+                raise IndexError(f"No predictions for status: {query['status']}")
+            print(self.master_df[f"{query['status']} Prediction"].notnull())
+            print(len(mask), len(self.master_df[f"{query['status']} Prediction"].notnull()))
+            mask = (mask) & (self.master_df[f"{query['status']} Prediction"].notnull())
+            print(Counter(self.master_df[f"{query['status']} Prediction"].notnull()))
+            print(mask)
+            st.write(f"status_filter: {Counter(mask)}")
+
+        st.write(f"status_filter: {Counter(mask)}")
+
+        if query['family'] != ['All Families'] and len(query['family'])> 0:
+            mask = (mask) & (self.master_df["family"].isin(query["family"]))
+            print(self.master_df["family"].isin(query["family"]))
+            print(Counter(self.master_df["family"].isin(query["family"])))
+        print(type(mask))
+        st.write(f"family_filter: {Counter(mask)}")
+        if query['order'] != ['All Orders'] and len(query['order']) > 0:
+            mask = (mask) | (self.master_df["order"].isin(query["order"]))
+        st.write(f"order_filter: {Counter(mask)}")
+
+        
+
+
+
+
+        if query['status'] != "All" and len(query['status']) > 0:
+            mask = mask & (self.master_df[f"{query['status']} Prediction Confidence"] > query["confidence"])
+
+
+        mask_df = self.master_df[mask]
+        st.dataframe(mask_df)
+        
 
 
 
@@ -123,26 +122,33 @@ class Dataview(CacheDataset): #TODO determine where to put queries
             {status : str,
              family: [vals],
              order: [vals], 
-             {status} Prediction: True/False,
+             {status} Prediction: True/False/None,
              {status} Prediction Confidence: float,
              confusion_state: ['True Positive', 'False Positive', 'True Negative', 'False Negative']}
         """
     
         # a status is necessary to see meaningful information for samples
-        if f"{query['status']} Prediction" not in self.fields:
-            raise IndexError(f"No predictions for status: {query['status']}")
+
 
 
         # work through the entries to build the query. Would sql be faster?
         mask = pd.Series([True] * len(self.master_df))
-        if query['family'] != '-':
+
+        if query['status'] != "All" and len(query['status']) > 0:
+            if f"{query['status']} Prediction" not in self.fields:
+                raise IndexError(f"No predictions for status: {query['status']}")
+            mask = mask & (self.master_df[f"{query['status']} Prediction"].notnull())
+
+        if query['family'] != 'All Families' and len(query['family'])> 0:
             mask = mask & (self.master_df["family"].isin(query["family"]))
-        if query['order'] != '-':
+        if query['order'] != 'All Orders' and len(query['order'])> 0:
             mask = mask & (self.master_df["family"].isin(query["family"]))
-        if query[f"{query['status']} Prediction"] != '-':
-            mask = mask & (self.master_df[f"{query['status']} Prediction"] == f"{query['status']} Prediction")
-        if query[f"{query['status']} Prediction Confidence"] != '-':
-            mask = mask & (self.master_df[f"{query['status']} Prediction Confidence"] > query[f"{query['status']} Prediction Confidence"])
+
+        # if query[f"{query['status']} Prediction"] != None:
+        #     mask = mask & (self.master_df[f"{query['status']} Prediction"] == f"{query['status']} Prediction")
+
+
+        mask = mask & (self.master_df[f"{query['status']} Prediction Confidence"] > query[f"{query['status']} Prediction Confidence"])
         if query[f"{query['status']} Prediction"]:
             def filter_conf_state(row, pred, gt):
                 return row[f"{query['status']} Prediction"] == pred and row[f"{query['status']} Ground Truth" ] == gt
@@ -162,7 +168,6 @@ class Dataview(CacheDataset): #TODO determine where to put queries
 
         
 
-        
 
     
     
