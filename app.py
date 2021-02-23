86f3bc022226fd11b7efab12506448cb0d5aa990
Anthony Jiang <anthony.jiang@yale.edu>
2021-02-23 15:00:38 -0500

    added recent data 2 scorings, cleaned directory

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ from src.views import Dataview
 from src.datasets import Metric
 import src.SessionState as ss
 from src.ConfirmButton import cache_on_button_press
+from src.scoring import ScoringSession
+from src.download_json import download_jsons
 
 password = os.environ.get('PASSWORD')
 password = "ytfc" 
@@ -29,7 +31,17 @@ datasets = OrderedDict(
     ("Dataset 1", "./data/dataset_info/dataset_1.json"),
     ("Dataset 2 - scored subset", "./data/dataset_info/dataset_2.json"),
     ("Dataset 2 - full predictions", "./data/dataset_info/dataset_2_full.json"),
+    ("Dataset 2 - 02-09-21 scored subset", "./data/dataset_info/dataset_2_02_09_21.json"),
     ("Dataset 3", "./data/dataset_info/dataset_3.json")]
+)
+
+load_mask = OrderedDict(
+    [("-", None),
+    ("Dataset 1", False),
+    ("Dataset 2 - scored subset", False),
+    ("Dataset 2 - full predictions", False),
+    ("Dataset 2 - 02-09-21 scored subset", True),
+    ("Dataset 3", False)]
 )
 
 mode_list = ["Homepage", "Inspection", "Scoring"]
@@ -73,17 +85,20 @@ def inspection():
     # returns the second element of the tuple with first value being the selection
 
     dataset_json = datasets[dataset_name]
+    req_load = load_mask[dataset_name]
 
     if dataset_name == "-":
         st.write("Welcome!")
         intro_mkd = Path("./assets/introduction.md").read_text()
         st.markdown(intro_mkd)
-
     else:
         print(loaded_dict.keys())
-        if dataset_name not in loaded_dict:
-            
+        if dataset_name not in loaded_dict:   
+
+
             dataset_info = load_info(dataset_json)
+
+            
             with st.spinner('Loading data...'):
                 # preprocessing is no longer done on client side
                 dataview = Dataview(dataset_info['status_list'], 
@@ -136,77 +151,6 @@ def inspection():
             response = requests.get(image_url)
             sample_image = Image.open(BytesIO(response.content))
             st.image(sample_image, caption = f"sample: {image_name}", use_column_width=True)
-
-class ScoringSession:
-
-    def __init__(self, session_key):
-        self.status_range = ["Reproductive", "Flowering", "Fruiting", "Budding"]
-        self.session_key = session_key
-        bucket.download_file(f"scoring_info/{session_key}/{session_key}.json", f"{session_key}.json")
-        with open(f"{session_key}.json", 'r') as session_json:
-            self.session = json.load(session_json)
-
-        self.session_name = self.session['session_name']
-        self.length = self.session['length']
-        self.index = ss.get(val = self.session['index'])
-
-        bucket.download_file(f'scoring_info/{session_key}/{session_key}_scoring.csv', f'{session_key}_scoring.csv')
-        self.scoring_df = pd.read_csv(f'{session_key}_scoring.csv')
-        
-        # bucket.download_file(f'scoring_info/{session_key}_comp.csv', f'{session_key}_comp.csv')
-
-
-    # replace this with a db!
-    def submit(self, status_dict):
-        self.session['index'] = status_dict['index']
-        
-
-        with open(f"{self.session_key}.json", 'w') as new_json:
-            json.dump(self.session, new_json)
-        bucket.upload_file(f"{self.session_key}.json", f"scoring_info/{self.session_key}/{self.session_key}.json")
-
-        with open(f"{self.session_key}_{status_dict['catalog_number']}.json", 'w') as new_json:
-            json.dump(status_dict, new_json)
-        bucket.upload_file(f"{self.session_key}_{status_dict['catalog_number']}.json", f"scoring_info/{self.session_key}/{status_dict['catalog_number']}.json")
-
-        self.index.val += 1
-
-    def score(self):
-        print(f"{self.index.val=}")
-
-        if self.index.val == self.session['length'] + 1:
-            st.write("Finished! Thank you :)")
-            st.stop()
-
-        col1, col2, col3, col4 = st.beta_columns(4)
-        status_dict = dict(zip(self.status_range, [np.NaN] * 4))
-        
-        with col1:    
-            status_dict['Reproductive'] = st.radio("Reproductive State:", [True, False, np.nan], index = 2)
-        with col2:
-            status_dict['Flowering']  = st.radio("Flowering State: ", [True, False, np.nan], index = 2)
-        with col3:
-            status_dict['Fruiting'] = st.radio("Fruiting State: ", [True, False, np.nan], index = 2)
-        with col4:
-            status_dict['Budding'] = st.radio("Budding State: ", [True, False, np.nan], index = 2)
-
-        submit = st.button("Submit")
-
-        if submit:
-            status_save = status_dict.copy()
-            status_save['catalog_number'] = self.scoring_df.loc[self.index.val, "catalog_number"]
-            status_save['index'] = self.index.val
-            self.submit(status_save)
-
-        image_url = self.scoring_df.loc[self.index.val, "url"]
-        image_name = self.scoring_df.loc[self.index.val, "catalog_number"]
-
-
-        response = requests.get(image_url)
-        sample_image = Image.open(BytesIO(response.content))
-        st.image(sample_image, caption = f"sample: {image_name}", use_column_width=True) 
-        st.text(f"{self.index.val}/{self.length} images scored")
-    
 
 def authentication():
     @cache_on_button_press('Authenticate')
--- src/scoring.py
+++ src/scoring.py
@@ -1,39 +1,32 @@
-import os, sys, shutil
 import json
+from collections import OrderedDict
+from pathlib import Path
+import os
 
-import requests
-from PIL import Image
+
+import streamlit as st
 import pandas as pd
 import numpy as np
-import streamlit as st
-import boto3
+from PIL import Image
+import requests
 from io import BytesIO
+import boto3
 
-import src.SessionState as ss
 
 bucket_name = "ytfc"
-
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(bucket_name)
 
-class ScoringInterface:
-
-    def __init__(self):
-        self.sessions = ["-", "02_09_2021"]
-
-
-    def show(self):
-        session_key = "02_09_2021"
-        session = ScoringSession(session_key)
-        session.score()
 
 
 
 class ScoringSession:
 
     def __init__(self, session_key):
-        bucket.download_file(f"scoring_info/{session_key}.json", f"{session_key}.json")
+        self.status_range = ["Reproductive", "Flowering", "Fruiting", "Budding"]
+        self.session_key = session_key
+        bucket.download_file(f"scoring_info/{session_key}/{session_key}.json", f"{session_key}.json")
         with open(f"{session_key}.json", 'r') as session_json:
             self.session = json.load(session_json)
 
@@ -41,42 +34,60 @@ class ScoringSession:
         self.length = self.session['length']
         self.index = ss.get(val = self.session['index'])
 
-        bucket.download_file(f'scoring_info/{session_key}_scoring.csv', f'{session_key}_scoring.csv')
+        bucket.download_file(f'scoring_info/{session_key}/{session_key}_scoring.csv', f'{session_key}_scoring.csv')
         self.scoring_df = pd.read_csv(f'{session_key}_scoring.csv')
         
-        bucket.download_file(f'scoring_info/{session_key}_comp.csv', f'{session_key}_comp.csv')
+        # bucket.download_file(f'scoring_info/{session_key}_comp.csv', f'{session_key}_comp.csv')
+
+
+    # replace this with a db!
+    def submit(self, status_dict):
+        self.session['index'] = status_dict['index']
+        
+
+        with open(f"{self.session_key}.json", 'w') as new_json:
+            json.dump(self.session, new_json)
+        bucket.upload_file(f"{self.session_key}.json", f"scoring_info/{self.session_key}/{self.session_key}.json")
+
+        with open(f"{self.session_key}_{status_dict['catalog_number']}.json", 'w') as new_json:
+            json.dump(status_dict, new_json)
+        bucket.upload_file(f"{self.session_key}_{status_dict['catalog_number']}.json", f"scoring_info/{self.session_key}/{status_dict['catalog_number']}.json")
+
+        self.index.val += 1
 
     def score(self):
-        st.button("hello!")
-        if st.button("forward"):
-            self.index.val +=1 
+        print(f"{self.index.val=}")
 
-        if st.button("backward"):
-            self.index.val -= 1
+        if self.index.val == self.session['length'] + 1:
+            st.write("Finished! Thank you :)")
+            st.stop()
 
+        col1, col2, col3, col4 = st.beta_columns(4)
+        status_dict = dict(zip(self.status_range, [np.NaN] * 4))
+        
+        with col1:    
+            status_dict['Reproductive'] = st.radio("Reproductive State:", [True, False, np.nan], index = 2)
+        with col2:
+            status_dict['Flowering']  = st.radio("Flowering State: ", [True, False, np.nan], index = 2)
+        with col3:
+            status_dict['Fruiting'] = st.radio("Fruiting State: ", [True, False, np.nan], index = 2)
+        with col4:
+            status_dict['Budding'] = st.radio("Budding State: ", [True, False, np.nan], index = 2)
+
+        submit = st.button("Submit")
+
+        if submit:
+            status_save = status_dict.copy()
+            status_save['catalog_number'] = self.scoring_df.loc[self.index.val, "catalog_number"]
+            status_save['index'] = self.index.val
+            self.submit(status_save)
 
         image_url = self.scoring_df.loc[self.index.val, "url"]
         image_name = self.scoring_df.loc[self.index.val, "catalog_number"]
-
 
 
         response = requests.get(image_url)
         sample_image = Image.open(BytesIO(response.content))
         st.image(sample_image, caption = f"sample: {image_name}", use_column_width=True) 
         st.text(f"{self.index.val}/{self.length} images scored")
-
-        
-
-        # get image loading code from the other place!
-
-        
-
-    def save(self):
-        save_button = st.button("Save")
-        if st.button("Save"):
-            # save scorings to s3, 
-            pass
-
-
-
-        
+    
