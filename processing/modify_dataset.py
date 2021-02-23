cb030c30446e0cd9325c8d5997b82e16caf8ae37
Anthony Jiang <anthony.jiang@yale.edu>
2021-02-23 10:59:33 -0500

    updated dataset 2, 3, added boto3 to requirements.txt

---
+++ processing/modify_dataset.py
@@ -0,0 +1,41 @@
+import sys
+
+import pandas as pd
+import numpy as np
+
+
+
+def update_field(target_df: str, update_df: str, update_map: dict):
+    # identify by the catalog_number
+    
+    update_df = update_df.rename(columns = update_map)
+    print([v for i,v in update_map.items()])
+    update_df = update_df[[v for i,v in update_map.items()]]
+    target_df.update(update_df)
+    join_df = pd.merge(target_df, update_df, how='inner', left_index=True,right_index=True)
+    print(join_df)
+    target_df.to_csv('updated_family.csv')
+
+
+def delete_entries(target_df, update_list):
+    target_df.drop(update_list, inplace=True, errors = 'ignore')
+    target_df.to_csv('updated.csv')
+
+
+if __name__ == "__main__":
+
+    target_path = sys.argv[1]
+    update_path =sys.argv[2]
+    delete_path = sys.argv[3]
+
+    target_df = pd.read_csv(target_path, index_col = 'catalog_number')
+    update_df = pd.read_csv(update_path, index_col = 'o.catalogNumber')
+    delete_df = pd.read_csv(delete_path, index_col = 'o.catalogNumber')
+    
+    delete_list = delete_df.index.values
+
+
+    update_field(target_df, update_df, {"accepted_family": "family", "o.scientificName": "scientific_name"})
+    delete_entries(target_df, delete_list)
+
+
