9919a2a3b95f742ecd8166c0e7110bb11377095a
Anthony Jiang <anthony.jiang@yale.edu>
2021-02-26 11:27:18 -0500

    new scoring dataset

--- processing/generate_scoring.py
+++ processing/generate_scoring.py
@@ -2,11 +2,9 @@ import random
 
 import pandas as pd
 import numpy as np
+df = pd.read_csv('/Users/antonsquared/Projects/ytfc_image_utility/data/filter_master/dataset_2_full.csv')
 
-
-def create_subsample(num):
-    df = pd.read_csv('/Users/antonsquared/Projects/ytfc_image_utility/data/filter_master/dataset_2_full.csv')
-
+def create_subsample(num, df):
     families = set(df['family'].dropna())
 
     sampling_dict = []
@@ -23,9 +21,22 @@ def create_subsample(num):
     comp_df = pd.DataFrame.from_dict(sampling_dict, orient='columns')
     for status in ["Reproductive", "Fruiting", "Flowering", "Budding"]:
         sample_df[f"{status} Ground Truth"] = np.nan
-    sample_df.to_csv('02_09_2021_scoring.csv')
-    comp_df.to_csv('02_09_2021_comp.csv')
+    # sample_df.to_csv('02_09_2021_scoring.csv')
+    # comp_df.to_csv('02_09_2021_comp.csv')
+    return sample_df, comp_df
+df_new, df_comp = create_subsample(10, df)
 
-create_subsample(5)
 
-# upload later using aws cli
+def exclude(df_old, df_new):
+    df_old.set_index(['cataldatog_number'], inplace = True)
+    df_new.set_index(['catalog_number'], inplace=True)
+    df_remains = df_new.drop(df_old.index.values, errors ='ignore')
+    return df_remains
+
+df_old = pd.read_csv('../02_09_2021_scoring.csv')
+
+df_new = exclude(df_old, df_new)
+df_new.reset_index(inplace=True)
+df_final, df_comp_final = create_subsample(5, df_new)
+df_final.to_csv('../dataset_2_02_24_2021_scoring.csv', index = True)
+df_comp_final.to_csv('../dataset_2_02_24_2021_comp.csv')
