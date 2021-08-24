d79d7afa12455cfe2b7e17f7c55acf34004b86db
Anthony Jiang <anthony.jiang@yale.edu>
2021-08-23 20:58:21 -0500

    WIP

--- analysis.py
+++ analysis.py
@@ -10,7 +10,7 @@ import json
 from collections import OrderedDict
 from pathlib import Path
 import os
-from typing import Union,List, Callable, Dict
+from typing import Union, List, Callable, Dict
 
 import streamlit as st
 import pandas as pd
@@ -33,22 +33,47 @@ password = "ytfc"
 
 
 datasets = OrderedDict([
-     ("Dataset 1", "./data/dataset_info/dataset_1.json"),
-     ("Dataset 1 - 06-05-retraining Test",
-      "./data/dataset_info/dataset_1_2021_06_05.json"),
-     ("Dataset 2 - scored subset", "./data/dataset_info/dataset_2.json"),
-     ("Dataset 2 - full predictions", "./data/dataset_info/dataset_2_full.json"),
-     ("Dataset 2 - 02-09-21 scored subset",
-      "./data/dataset_info/dataset_2_02_09_21.json"),
-     ("Dataset 3", "./data/dataset_info/dataset_3.json"),
-     ("Dataset 3 - full - old_model", "./data/dataset_info/dataset_3_2021_06_15.json")]
+    ("Dataset 1", "./data/dataset_info/dataset_1.json"),
+    ("Dataset 1 - 06-05-retraining Test",
+     "./data/dataset_info/dataset_1_2021_06_05.json"),
+    ("Dataset 2 - scored subset", "./data/dataset_info/dataset_2.json"),
+    ("Dataset 2 - full predictions", "./data/dataset_info/dataset_2_full.json"),
+    ("Dataset 2 - 02-09-21 scored subset",
+     "./data/dataset_info/dataset_2_02_09_21.json"),
+    ("Dataset 3", "./data/dataset_info/dataset_3.json"),
+    ("Dataset 3 - full - old_model", "./data/dataset_info/dataset_3_2021_06_15.json")]
 )
 
 
 mode_list = ["Homepage", "Inspection"]
+DEFAULT_COLORS = [
+    '#1f77b4',  # muted blue
+    '#ff7f0e',  # safety orange
+    '#2ca02c',  # cooked asparagus green
+    '#d62728',  # brick red
+    '#9467bd',  # muted purple
+    '#8c564b',  # chestnut brown
+    '#e377c2',  # raspberry yogurt pink
+    '#7f7f7f',  # middle gray
+    '#bcbd22',  # curry yellow-green
+    '#17becf'   # blue-teal
+]
 
+DEFAULT_LINE_TYPES = ["solid", "dot", "dash",
+                      "longdash", "dashdot", "longdashdot"]
+METRIC_LINE_MAP = {"Accuracy": 'solid',
+                   "Capture": 'dot'}
 
 status_list = ['Budding', 'Flowering', 'Fruiting', 'Reproductive']
+
+STATUS_COLOR_MAP = {"Budding": "#ff7f0e",
+                    "Flowering": '#2ca02c',
+                    "Fruiting": '#17becf',
+                    "Reproductive": '#9467bd'}
+
+
+TITLE_MAP = {"Dataset 3 - full - old_model": "Capture percentages on taxa not represented in the Training set",
+            "Dataset 1 - 06-05-retraining Test": "Accuracy and Capture on validation dataset"}
 
 status = None
 
@@ -56,8 +81,6 @@ bucket_name = "ytfc"
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(bucket_name)
-
-
 
 
 def load_all():
@@ -72,36 +95,78 @@ def load_all():
         return Dataview(dataset_info['status_list'],
                         dataset_info['base_schema'], dataset_info['master_path'])
 
+    return {k: process_json(v) for k, v in datasets.items()}
 
-    return {k: process_json(v) for k,v in datasets.items()}
 
 def make_accuracy_recall_df(dv: Dataview, metrics):
     """
     Compute statistics by family
-    
+
     """
 
-    # famil
-    family_arr = pd.Series(dv.master_df['family'].unique(),dtype=object)
-
+    # family
+    family_arr = pd.Series(dv.master_df['family'].unique(), dtype=object)
 
     # order
     order_list = list(dv.order_map.values()) + ["All Orders"]
 
-    full_metrics = ['Accuracy %', 'Capture %', 'Count', 'Ground Truth Positive %', 'Ground Truth Negative %', 'Ground Truth Undetermined %', 'True Positive %', 'False Positive %', 'False Negative %', 'True Negative %']
+    full_metrics = ['Accuracy %', 'Capture %', 'Count', 'Ground Truth Positive %', 'Ground Truth Negative %',
+                    'Ground Truth Undetermined %', 'True Positive %', 'False Positive %', 'False Negative %', 'True Negative %']
     # metrics
-    selected_metrics = st.multiselect("Select the metrics to chart", full_metrics, ["Accuracy %", "Capture %"])
+    selected_metrics = st.multiselect(
+        "Select the metrics to chart", full_metrics, ["Accuracy %", "Capture %"])
 
-
-    base_metric_df, mask_df = dv.summary_pd_query({"status": status_list, "family": family_arr, "order": order_list}, metrics = metrics)
+    base_metric_df, mask_df = dv.summary_pd_query(
+        {"status": status_list, "family": family_arr, "order": order_list}, metrics=metrics)
     return base_metric_df
 
 
+def make_accuracy_recall_curve_long(key, df: pd.DataFrame):
+    df.to_csv(f"./reports/{key}_premelt.csv")
+    statuses = ["Reproductive", "Flowering", "Fruiting", "Budding"]
+    df.index.name = "Threshold Percent"
+    long_df = df.melt(var_name="Measurement",
+                      value_name="Percentage", ignore_index=False).reset_index()
+    long_df.to_csv(f"./reports/{key}_melt.csv")
+    return long_df
+
+
 def make_accuracy_recall_curve(key: str, df: pd.DataFrame):
-    # statuses = ["Reproductive", "Flowering", "Fruiting", "Budding"]
-    fig = px.line(df)
+
+    statuses = ["Reproductive", "Flowering", "Fruiting", "Budding"]
+    # determine if we're including accuracy or not
+    print(f"DF COLUMNS: {key}")
+    print(df.columns)
+    accuracy_group = []
+    capture_group = []
+    for s in statuses:
+        if f"{s} Accuracy" in df.columns:
+            accuracy_group.append(f"{s} Accuracy")
+        if f"{s} Capture" in df.columns:
+            capture_group.append(f"{s} Capture")
+    color_discrete_map = {}
+    line_dash_map = {}
+    # build color and line maps
+
+    for c in df.columns:
+        status, metric = c.split(' ')
+        color_discrete_map[c] = STATUS_COLOR_MAP[status]
+        line_dash_map[c] = METRIC_LINE_MAP[metric]
+
+    long_df = make_accuracy_recall_curve_long(key, df)
+    print(line_dash_map)
+    print(color_discrete_map)
+    try:
+        key_title = TITLE_MAP[key]
+    except KeyError:
+        key_title = None
+    
+    fig = px.line(long_df, x='Threshold Percent', y='Percentage', color='Measurement',
+                  color_discrete_map=color_discrete_map, line_dash="Measurement", line_dash_map=line_dash_map,
+                  title=key_title)
     if not os.path.isdir(f"/Users/antonsquared/Projects/ytfc_image_utility/reports"):
-        os.path.makedirs(f"/Users/antonsquared/Projects/ytfc_image_utility/reports")
+        os.path.makedirs(
+            f"/Users/antonsquared/Projects/ytfc_image_utility/reports")
     fig.write_html(f"./reports/{key}.html")
 
 
@@ -115,60 +180,75 @@ def taxa_count(df, taxa_col, truncate: Union[int, float, bool]):
                      if float (and less than 1): then use this as the threshold for inclusion in the "OTHER" column
                      if bool and FALSE: no truncation
                      as a consequence: floats greater than 1 and TRUE raise errors
-    
+
     """
     df['Counts'] = np.zeros(len(df))
     gp_df = df.groupby([taxa_col])[taxa_col, "Counts"].count()
     print(gp_df.head(3))
-    gp_df.loc[:, "\% of total"] = gp_df.loc[:, "Counts"]/len(df) 
-
+    gp_df.loc[:, "\% of total"] = gp_df.loc[:, "Counts"]/len(df)
 
     if isinstance(truncate, int):
         raise NotImplementedError
     elif isinstance(truncate, float):
         truncate_df = gp_df[gp_df["\% of total"] > truncate]
-        truncate_df.loc["OTHER"] = {"family": np.NaN, "Counts": len(df) - len(truncate_df)}
+        
+        truncate_df.loc["OTHER"] = {
+            "family": np.NaN, "Counts": len(df) - truncate_df["Counts"].sum(), "\% of total": (len(df)- truncate_df["Counts"].sum())/len(df)}
+        # truncate_df.drop(columns = ['order'])
+        truncate_df.drop(columns=['order'], inplace=True)
+        truncate_df.sort_values(by = "Counts", ascending = False, inplace=True)
         return truncate_df
     elif isinstance(truncate, bool):
         return gp_df
     else:
-        raise TypeError(f"{truncate} is of invalid type; the value for truncate must be a integer, float, or bool")
-        
-
-
+        raise TypeError(
+            f"{truncate} is of invalid type; the value for truncate must be a integer, float, or bool")
 
 
 if __name__ == "__main__":
     datasets_dict = load_all()
+
+    training_df = pd.read_csv("/Users/antonsquared/Projects/ytfc_image_utility/data/training_datasets/dataset_1_train.csv")
+    output_df = taxa_count(training_df, "family", 0.01)
+    output_df.drop(columns = ["family"], inplace=True)
+    output_df["\% of total"] = output_df["\% of total"] * 100
+    with open(f"dataset_1_training.tex", 'w') as out_tex:
+        out_tex.write(output_df.to_latex(float_format="%.2f"))
+
+
     # datasets_dict.__delitem__("-")
     for k, v in datasets_dict.items():
 
-
         # accuracy/capture graphs
         try:
-            df = make_accuracy_recall_df(v, metrics = ['Accuracy %', 'Capture %'])
+            df = make_accuracy_recall_df(
+                v, metrics=['Accuracy %', 'Capture %'])
         except Exception as E:
-            df = make_accuracy_recall_df(v, metrics = [ 'Capture %'])
+            df = make_accuracy_recall_df(v, metrics=['Capture %'])
         make_accuracy_recall_curve(k, df)
-        
 
         # taxa stats
         # try:
-        gp_df = taxa_count(v.master_df, "family", truncate = 0.02)
-        gp_df.to_csv(f"/Users/antonsquared/Projects/ytfc_image_utility/reports/{k}_summary_by_taxa.csv")
+        gp_df = taxa_count(v.master_df, "order", truncate=0.02)
+        gp_df.to_csv(
+            f"/Users/antonsquared/Projects/ytfc_image_utility/reports/{k}_summary_by_order.csv")
+        with open(f"{k}.tex", 'w') as out_tex:
+            out_tex.write(gp_df.to_latex(float_format="%.2f"))
+            
         # except Exception as E:
         #     print(f"dataset:{k} - gp_df")
         #     print(E)
 
-
         # stats by taxa
         try:
-            stats_by_taxa_df = v.stats_by_taxa("family", status_list, metrics=['Accuracy %', 'Capture %', "Count"])
+            stats_by_taxa_df = v.stats_by_taxa("order", status_list, metrics=[
+                                               'Accuracy %', 'Capture %', "Count"])
         except KeyError as E:
             print(f"dataset:{k} - stats_by_taxa")
             print(E)
-            stats_by_taxa_df = v.stats_by_taxa("family", status_list, metrics=['Capture %', "Count"])
-        stats_by_taxa_df.to_csv(f"/Users/antonsquared/Projects/ytfc_image_utility/reports/{k}_stats_by_taxa.csv")
-
+            stats_by_taxa_df = v.stats_by_taxa(
+                "order", status_list, metrics=['Capture %', "Count"])
+        stats_by_taxa_df.to_csv(
+            f"/Users/antonsquared/Projects/ytfc_image_utility/reports/{k}_stats_by_order.csv")
 
         # composition graphs:
--- processing/modify_dataset.py
+++ processing/modify_dataset.py
@@ -8,19 +8,18 @@ May need to manually adjust indexes
 
 
 Usage:
-    modify_dataset.py delete <target_file> <delete_file>
-    modify_dataset.py update <target_file> <update_file>
-
-
+    modify_dataset.py delete <target_file> <delete_file> <output_file_name>
+    modify_dataset.py update <target_file> <update_file> <output_file_name>
+    modify_dataset.py test-delete <target_file> <delete_file>
+    modify_dataset.py test-update <target_file> <update_file>
 
 """
-
+from typing import List
 import sys
 
 import pandas as pd
 import numpy as np
 import docopt
-
 
 
 def update_field(target_df: str, update_df: str, update_map: dict):
@@ -31,10 +30,10 @@ def update_field(target_df: str, update_df: str, update_map: dict):
 
     """
     # identify by the catalog_number
-    
-    update_df = update_df.rename(columns = update_map)
-    print([v for i,v in update_map.items()])
-    update_df = update_df[[v for i,v in update_map.items()]]
+
+    update_df = update_df.rename(columns=update_map)
+    print([v for i, v in update_map.items()])
+    update_df = update_df[[v for i, v in update_map.items()]]
 
     # dedup inputs
     print(len(update_df), len(target_df))
@@ -55,22 +54,26 @@ def update_field(target_df: str, update_df: str, update_map: dict):
     # update_df.index.drop_duplicates
     target_df.loc[target_df.index.duplicated(), :].to_csv("duplicates.csv")
 
+    # manually update:
 
-    ## manually update:
+    target_df.update(update_df, overwrite=True, errors='ignore')
 
-    target_df.update(update_df, overwrite = True, errors = 'ignore')
-
-
-    target_df.to_csv('updated_family.csv')
+    return target_df
 
 
 def delete_entries(target_df, update_list):
-    pruned_df =target_df.drop(update_list, errors = 'ignore')
+    pruned_df = target_df.drop(update_list, errors='ignore')
     pruned_df.index.name = "o.CNH_id"
-    pruned_df.to_csv('updated_3.csv')
+    return pruned_df
 
 
+def test_deleted_entries(target_df: pd.DataFrame, delete_list: List[str]):
+    """
+    Verifies that no elements of delete_list exist in target_df
 
+    """
+    assert len(set(target_df.index) & set(delete_list)) == 0
+    print("Assertion passed!")
 
 if __name__ == "__main__":
     args = docopt.docopt(__doc__)
@@ -80,29 +83,57 @@ if __name__ == "__main__":
         # update_path =sys.argv[2]
         delete_path = args["<delete_file>"]
 
-        target_df = pd.read_csv(target_path, index_col = 'object_id')
+        target_df = pd.read_csv(target_path, index_col='object_id')
         # update_df = pd.read_csv(update_path, index_col = 'o.catalogNumber')
-        delete_df = pd.read_csv(delete_path, index_col = 'occid')
-        
+        delete_df = pd.read_csv(delete_path, index_col='occid')
+
         delete_list = delete_df.index.values
 
-
         # update_field(target_df, update_df, {"accepted_family": "family", "o.scientificName": "scientific_name"})
-        delete_entries(target_df, delete_list)
-    
+        output_df = delete_entries(target_df, delete_list)
+        test_deleted_entries(output_df, delete_list)
+
     elif args["update"]:
         target_path = args["<target_file>"]
         update_path = args["<update_file>"]
 
-        target_df = pd.read_csv(target_path, index_col = 'object_id')
-        update_df = pd.read_csv(update_path, index_col = 'o.CNH_id')
+        target_df = pd.read_csv(target_path, index_col='object_id')
+        update_df = pd.read_csv(update_path, index_col='o.CNH_id')
         # delete_df = pd.read_csv(delete_path, index_col = 'catalogNumber')
-        
+
         # delete_list = delete_df.index.values
 
+        output_df = update_field(target_df, update_df, {
+                                 "accepted_family": "o.family", "o.scientificName": "o.scientificName"})
+        
+        output_df.to_csv(f"{args['<output_file_name>']}")
+        
+    elif args["test-delete"]:
+        target_path = args["<target_file>"]
+        # update_path =sys.argv[2]
+        delete_path = args["<delete_file>"]
 
-        update_field(target_df, update_df, {"accepted_family": "o.family", "o.scientificName": "o.scientificName"})
+        target_df = pd.read_csv(target_path, index_col='o.CNH_id')
+        # update_df = pd.read_csv(update_path, index_col = 'o.catalogNumber')
+        delete_df = pd.read_csv(delete_path, index_col='occid')
 
+        delete_list = delete_df.index.values
 
+        # update_field(target_df, update_df, {"accepted_family": "family", "o.scientificName": "scientific_name"})
+        # output_df = delete_entries(target_df, delete_list)
+        test_deleted_entries(target_df, delete_list)
 
+    elif args["test-rename"]:
+        target_path = args["<target_file>"]
+        update_path = args["<update_file>"]
 
+        target_df = pd.read_csv(target_path, index_col='object_id')
+        update_df = pd.read_csv(update_path, index_col='o.CNH_id')
+        # delete_df = pd.read_csv(delete_path, index_col = 'catalogNumber')
+
+        # delete_list = delete_df.index.values
+
+        output_df = update_field(target_df, update_df, {
+                                 "accepted_family": "o.family", "o.scientificName": "o.scientificName"})
+        
+        output_df.to_csv(f"{args['<output_file_name>']}")
